fbcab8fe944fc54b2554d3dcbc472a851fb55798
Assessment External <assessment-104@external.com.tr>
2021-09-11 17:08:59 +0300

    HomeViewModelTest Class added

--- app/src/test/java/example/com/moviedb/HomeViewModelTest.kt
+++ app/src/test/java/example/com/moviedb/HomeViewModelTest.kt
@@ -1,4 +1,49 @@
 package example.com.moviedb
 
+import example.com.moviedb.features.fav.db.FavRepository
+import example.com.moviedb.features.fav.model.FavModel
+import example.com.moviedb.features.home.HomeViewModel
+import example.com.moviedb.features.home.PopularMovieListSource
+import org.junit.Before
+import org.junit.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+
+
+
+
+
 class HomeViewModelTest {
+   @Mock
+    var source: PopularMovieListSource? = null
+    @Mock
+    var repo: FavRepository? = null
+
+    @InjectMocks
+    var viewmodel: HomeViewModel? = null
+    @Test
+    fun testDeleteMovie() {
+        val model = FavModel("1", "Test")
+        viewmodel?.deleteMovie(model)
+    }
+    @Test
+    fun testSearchByMovieName() {
+        viewmodel?.searchByMovieName("test")
+    }
+    @Test
+    fun testGetAllList() {
+        viewmodel?.getAllList()
+    }
+    @Test
+    fun testCheckById() {
+        viewmodel?.checkById("model")
+    }
+
+    @Test
+    fun testGetPopularMovieList() {
+        viewmodel?.getAllList()
+    }
+
 }
